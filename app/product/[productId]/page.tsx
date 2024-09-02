ee5081e84f2179d33b4ebd112287069fa281937f
athuljain <athuljainkj2@gmail.com>
2024-09-02 08:43:31 +0530

    nextjs

--- app/product/[productId]/page.tsx
+++ app/product/[productId]/page.tsx
@@ -2,15 +2,34 @@
 
 import { useParams } from "next/navigation"
 
-export default function ProductId(){
-    const {productId}=useParams()
-    console.log("productID",productId);
-    
-    return(
-        <div>
-            
-<h1>Product Id: {productId}</h1>
 
+// this code do js code
+
+// export default function ProductId(){
+//     const {productId}=useParams()
+//     console.log("productID",productId);
+    
+//     return(
+//         <div>
+
+// <h1>Product Id: {productId}</h1>
+
+//         </div>
+//     )
+// }
+
+
+
+// this is typescript code
+
+
+export default function ProductId({ params }: { params: { productId: string } }) {
+    const { productId } = params;
+
+    
+    return (
+        <div>
+            <h1>Product Id: {productId}</h1>
         </div>
-    )
+    );
 }
